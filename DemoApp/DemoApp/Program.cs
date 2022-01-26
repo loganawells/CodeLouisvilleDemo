b7114f1569015bafead89d9f6a79f2343ce28143
Logan Wells <loganawells@gmail.com>
2022-01-26 08:55:02 -0500

    Moved functions to static class

--- DemoApp/DemoApp/Program.cs
+++ DemoApp/DemoApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Text;
+using DemoApp.Utilities;
 
 namespace DemoApp
 {
@@ -7,49 +8,13 @@ namespace DemoApp
 	{
 		static void Main(string[] args)
 		{
-			string fullAlphabet = GenerateAlphabetString();
-			string backwardAlphabet = GenerateBackwardAlphabetString();
-			string skippedAlphabet = GenerateSkippedAlphabetString();
+			string fullAlphabet = AlphabetUtility.GenerateAlphabetString();
+			string backwardAlphabet = AlphabetUtility.GenerateBackwardAlphabetString();
+			string skippedAlphabet = AlphabetUtility.GenerateSkippedAlphabetString();
 
 			Console.WriteLine($"Full alphabet: {fullAlphabet}");
 			Console.WriteLine($"Backward alphabet: {backwardAlphabet}");
 			Console.WriteLine($"Skipped alphabet: {skippedAlphabet}");
-		}
-
-		private static string GenerateAlphabetString()
-		{
-			var alphabet = new StringBuilder();
-
-			for (char letter = 'A'; letter <= 'Z'; letter++)
-			{
-				alphabet.Append(letter);
-			}
-
-			return alphabet.ToString();
-		}
-
-		private static string GenerateBackwardAlphabetString()
-		{
-			var alphabet = new StringBuilder();
-
-			for (char letter = 'Z'; letter >= 'A'; letter--)
-			{
-				alphabet.Append(letter);
-			}
-
-			return alphabet.ToString();
-		}
-
-		private static string GenerateSkippedAlphabetString()
-		{
-			var alphabet = new StringBuilder();
-
-			for (char letter = 'A'; letter <= 'Z'; letter++, letter++)
-			{
-				alphabet.Append(letter);
-			}
-
-			return alphabet.ToString();
 		}
 	}
 }
---
+++ DemoApp/DemoApp/Utilities/AlphabetUtility.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp.Utilities
+{
+	public static class AlphabetUtility
+	{
+		private const char _firstLetter = 'A';
+		private const char _lastLetter = 'Z';
+
+		public static string GenerateAlphabetString()
+		{
+			var alphabet = new StringBuilder();
+
+			for (char letter = _firstLetter; letter <= _lastLetter; letter++)
+			{
+				alphabet.Append(letter);
+			}
+
+			return alphabet.ToString();
+		}
+
+		public static string GenerateBackwardAlphabetString()
+		{
+			var alphabet = new StringBuilder();
+
+			for (char letter = _lastLetter; letter >= _firstLetter; letter--)
+			{
+				alphabet.Append(letter);
+			}
+
+			return alphabet.ToString();
+		}
+
+		public static string GenerateSkippedAlphabetString()
+		{
+			var alphabet = new StringBuilder();
+
+			for (char letter = _firstLetter; letter <= _lastLetter; letter++, letter++)
+			{
+				alphabet.Append(letter);
+			}
+
+			return alphabet.ToString();
+		}
+	}
+}
