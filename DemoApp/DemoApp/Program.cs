0361062f8bf6cd0c9b35af09eba9e9e3d6d96c2a
Logan Wells <loganawells@gmail.com>
2022-01-25 20:10:41 -0500

    Added backward alphabet and skipped alphabet

--- DemoApp/DemoApp/Program.cs
+++ DemoApp/DemoApp/Program.cs
@@ -7,9 +7,13 @@ namespace DemoApp
 	{
 		static void Main(string[] args)
 		{
-			string alphabet = GenerateAlphabetString();
+			string fullAlphabet = GenerateAlphabetString();
+			string backwardAlphabet = GenerateBackwardAlphabetString();
+			string skippedAlphabet = GenerateSkippedAlphabetString();
 
-			Console.WriteLine(alphabet);
+			Console.WriteLine($"Full alphabet: {fullAlphabet}");
+			Console.WriteLine($"Backward alphabet: {backwardAlphabet}");
+			Console.WriteLine($"Skipped alphabet: {skippedAlphabet}");
 		}
 
 		private static string GenerateAlphabetString()
@@ -17,6 +21,30 @@ namespace DemoApp
 			var alphabet = new StringBuilder();
 
 			for (char letter = 'A'; letter <= 'Z'; letter++)
+			{
+				alphabet.Append(letter);
+			}
+
+			return alphabet.ToString();
+		}
+
+		private static string GenerateBackwardAlphabetString()
+		{
+			var alphabet = new StringBuilder();
+
+			for (char letter = 'Z'; letter >= 'A'; letter--)
+			{
+				alphabet.Append(letter);
+			}
+
+			return alphabet.ToString();
+		}
+
+		private static string GenerateSkippedAlphabetString()
+		{
+			var alphabet = new StringBuilder();
+
+			for (char letter = 'A'; letter <= 'Z'; letter++, letter++)
 			{
 				alphabet.Append(letter);
 			}
