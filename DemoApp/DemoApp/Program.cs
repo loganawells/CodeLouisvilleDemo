11ad4ecd4b6233c89fbc4a327284b2a46147797e
Logan Wells <loganawells@gmail.com>
2022-02-08 19:19:17 -0500

    Added a menu and menu loop

--- DemoApp/DemoApp/Program.cs
+++ DemoApp/DemoApp/Program.cs
@@ -8,13 +8,46 @@ namespace DemoApp
 	{
 		static void Main(string[] args)
 		{
-			string fullAlphabet = AlphabetUtility.GenerateAlphabetString();
-			string backwardAlphabet = AlphabetUtility.GenerateBackwardAlphabetString();
-			string skippedAlphabet = AlphabetUtility.GenerateSkippedAlphabetString();
+			DisplayMenu();
+			string userInput = Console.ReadLine().ToLower();
 
-			Console.WriteLine($"Full alphabet: {fullAlphabet}");
-			Console.WriteLine($"Backward alphabet: {backwardAlphabet}");
-			Console.WriteLine($"Skipped alphabet: {skippedAlphabet}");
+			while (userInput != "q")
+			{
+				switch (userInput)
+				{
+					case "1":
+						Console.WriteLine(AlphabetUtility.GenerateAlphabetString());
+						break;
+					case "2":
+						Console.WriteLine(AlphabetUtility.GenerateBackwardAlphabetString());
+						break;
+					case "3":
+						Console.Write("Print every Nth letter. Enter a value for N: ");
+						string skipInput = Console.ReadLine();
+						Console.WriteLine($"Not implemented. Printing every 2nd letter...");
+						Console.WriteLine(AlphabetUtility.GenerateSkippedAlphabetString());
+						break;
+					default:
+						break;
+				}
+
+				Console.WriteLine("Press enter to return to the menu.");
+				Console.ReadLine();
+				Console.Clear();
+				DisplayMenu();
+				userInput = Console.ReadLine().ToLower();
+			}
+		}
+
+		static void DisplayMenu()
+		{
+			Console.WriteLine("Welcome to the Code Louisville Alphabet Demo App!");
+			Console.WriteLine("Options:");
+			Console.WriteLine("  1 - Print full alphabet");
+			Console.WriteLine("  2 - Print full alphabet backwards");
+			Console.WriteLine("  3 - Print alphabet every Nth letter");
+			Console.WriteLine("  q - Exit");
+			Console.Write("Enter your selection: ");
 		}
 	}
 }
