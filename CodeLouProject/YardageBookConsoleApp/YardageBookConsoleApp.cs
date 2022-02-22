383c3e612ba3005e5203608d3a8e135b4a5c3e2f
Logan Wells <loganawells@gmail.com>
2022-02-22 15:03:54 -0500

    Cleaned up

--- CodeLouProject/YardageBookConsoleApp/YardageBookConsoleApp.cs
+++ CodeLouProject/YardageBookConsoleApp/YardageBookConsoleApp.cs
@@ -1,9 +1,6 @@
 ﻿using CodeLouisvilleLibrary;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using YardageBook;
 
 namespace YardageBookConsoleApp
